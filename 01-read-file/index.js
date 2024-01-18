245391d233f31e555f04d4bf5f61bdf2578f8047
mrrobot911 <kelwin2705@gmail.com>
2024-01-18 16:21:25 +0300

    fix: 1 task output

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const file = path.resolve('./01-read-file/text.txt');
-const reader = fs.createReadStream(file);
+const file = path.join('01-read-file', 'text.txt');
+const reader = fs.createReadStream(path.resolve(file), { highWaterMark: 8 });
 reader.on('error', (err) => {
   console.error('Error:', err);
 });
-reader.on('data', (chunk) => console.log(chunk.toString()));
+reader.on('data', (chunk) => process.stdout.write(chunk.toString()));
